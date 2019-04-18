b160128fec508eb4ac3333a7913695b158578c4d
Thuy Nguyen <bienthuy@hotmail.com>
2019-04-17 23:58:11 -0700

    Bài 7

---
+++ ToanTu.py
@@ -0,0 +1,17 @@
+x = 5
+y = x ** 2
+print(x)
+print(y)
+
+year = 2018
+t = year % 2 == 0 and year % 3 > 1
+print(t)
+t = year % 2 == 0 or year % 3 > 5
+print(t)
+print('*' * 20)
+print(5 is not 5)
+print(4 is 2**2)
+
+print(9 / 2)
+print(9 // 2)
+print(9 % 2)

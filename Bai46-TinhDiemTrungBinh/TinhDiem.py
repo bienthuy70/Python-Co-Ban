4810d9a955b0b0519a2b606ff69553c1f3a92598
thang39 <47665780+thang39@users.noreply.github.com>
2019-04-25 09:57:14 +0700

    Bai 46

--- Bai46-TinhDiemTrungBinh/TinhDiem.py
+++ Bai46-TinhDiemTrungBinh/TinhDiem.py
@@ -2,5 +2,5 @@ toan, ly, hoa = eval(input("Nhập điểm Toán, Lý, Hóa: "))
 print("Điểm Toán:", toan)
 print("Điểm Lý:", ly)
 print("Điểm Hóa:", hoa)
-dtb=(toan+ly+hoa)/3
-print("Điểm trung bình =",round(dtb,2))
+dtb = (toan + ly + hoa) / 3
+print("Điểm trung bình =", round(dtb, 2))

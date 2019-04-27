dcddf6b513ed1e5a5c84c662ae7310e1ced7cff5
thang39 <47665780+thang39@users.noreply.github.com>
2019-04-27 16:51:18 +0700

    Bai 70

--- Bai70-BaiTapRenLuyen/ToiUuChuoi.py
+++ Bai70-BaiTapRenLuyen/ToiUuChuoi.py
@@ -2,4 +2,16 @@ def ToiUuChuoi(s):
     s = s.strip()
     return s
 
-print(ToiUuChuoi("   Nguyen   Vu Bien   Thuy   "))
+
+s = "   Nguyen   Vu Bien   Thuy   "
+s = s.strip()
+x = s.split(" ")
+while True:
+    if "" in x:
+        x.remove("")
+    else:
+        break
+print(x)
+s2 = " "
+s2 = s2.join(x)
+print(s2)

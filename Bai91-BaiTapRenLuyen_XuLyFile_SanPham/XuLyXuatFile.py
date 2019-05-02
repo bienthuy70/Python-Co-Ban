a230d924d6322589dcb9c1663a73e0b7eceae5bd
Thuy Nguyen <newuser886@yahoo.com>
2019-05-02 16:26:57 +0700

    Bai 91

--- Bai91-BaiTapRenLuyen_XuLyFile_SanPham/XuLyXuatFile.py
+++ Bai91-BaiTapRenLuyen_XuLyFile_SanPham/XuLyXuatFile.py
@@ -1,18 +1,31 @@
-def DocFile(path):
+# Đọc File Lưu vào List
+def FileToList(path: str, index: int = 2) -> list:
     File = open(path, 'r', encoding='utf-8')
-    print("-"*77)
-    print("| {0:<10}| {1:<50}|{2:>10} |".format("Mã SP", "Tên SP", "Đơn Giá"))
-    print("-"*77)
+    lst = []
     for line in File:
-        data = (line.strip()).split(';')
-        print("| {0:<10}| {1:<50}|{2:>10} |".format(data[0], data[1], data[2]))
-    File.close()
-    print("-"*77)
+        data = line.strip().split(';')
+        data[index] = float(data[index])
+        lst.append(data)
+    return lst
 
 
-DocFile("sanpham.txt")
-lst=[]
-lst.sort(key=)
+def RecordSort(lst: list, index: int = 0, mode: int = 0):
+    for i in range(len(lst)):
+        for j in range(len(lst)):
+            a = lst[i]
+            b = lst[j]
+            if mode is 0:
+                if a[index] < b[index]:
+                    lst[i] = b
+                    lst[j] = a
+            else:
+                if a[index] > b[index]:
+                    lst[i] = b
+                    lst[j] = a
 
 
 # Sắp xếp theo theo đơn giá giảm dần
+lst = FileToList("sanpham.txt")
+print(lst)
+RecordSort(lst, 2, 1)
+print(lst)

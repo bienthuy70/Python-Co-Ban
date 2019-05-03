8d16f9b5c6389647c4ae45d52063eb757a611f02
Thuy Nguyen <newuser886@yahoo.com>
2019-05-03 17:05:15 +0700

    Bai 91

--- Bai93-CacBaiTapTuRenLuyen/QuanLySanPham.py
+++ Bai93-CacBaiTapTuRenLuyen/QuanLySanPham.py
@@ -5,3 +5,7 @@
 - Cho phép thêm mới, sửa, xóa, tiềm kiếm, sắp xếp, lưu và đọc Text File
 """
 print("Phần mềm Quản Lý Sản Phẩm")
+print("Nhập danh mục nhóm sản phẩm")
+masp = input("Nhập Mã Nhóm: ")
+tensp = input("Nhập Tên Nhóm: ")
+print(masp, tensp)

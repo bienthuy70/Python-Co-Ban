91ee3fac92e45fdae5d6ad9822a80f74eb57a80d
thang39 <47665780+thang39@users.noreply.github.com>
2019-05-05 23:26:43 +0700

    Bài 93

--- Bai93-CacBaiTapTuRenLuyen/QuanLySanPham.py
+++ Bai93-CacBaiTapTuRenLuyen/QuanLySanPham.py
@@ -4,8 +4,31 @@
 - Mỗi Sản Phẩm có: Mã, Tên, Đơn giá; Mỗi một sản phẩm thuộc về một danh mục
 - Cho phép thêm mới, sửa, xóa, tiềm kiếm, sắp xếp, lưu và đọc Text File
 """
+import mysql.connector
+
+
+def ThemDanhMuc(val):
+    print("Đang kết nối...")
+    mydb = mysql.connector.connect(host='localhost', user='qlsp', passwd='khongco', database='qlsp')
+    mycursor = mydb.cursor()
+    print("Kết nối thành công!")
+    sql = "INSERT INTO dmnhom (manhom, tennhom) VALUES (%s, %s)"
+    print("Đang thêm...")
+    mycursor.executemany(sql, val)
+    mydb.commit()
+    print("Đã thêm thành công", mycursor.rowcount, "mẫu tin")
+    mydb.close()
+
+
 print("Phần mềm Quản Lý Sản Phẩm")
 print("Nhập danh mục nhóm sản phẩm")
-masp = input("Nhập Mã Nhóm: ")
-tensp = input("Nhập Tên Nhóm: ")
-print(masp, tensp)
+val = []
+while True:
+    manhom = input("Nhập Mã Nhóm: ")
+    tennhom = input("Nhập Tên Nhóm: ")
+    val.append((manhom.strip(), tennhom.strip()))
+    tieptuc = input("Tiếp tục (c/k)? ")
+    if tieptuc is 'k':
+        break
+print(val)
+ThemDanhMuc(val)

f5048206d87198e8e01030c8df31b3e01bbc16c7
Thuy Nguyen <newuser886@yahoo.com>
2019-05-14 17:08:09 +0700

    Bai 95

---
+++ Bai95-GiaoDienTkinter/TaoCuaSo.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from centerlib import makecenter
+
+root = Tk()
+root.title("Hello Python Tkinter")
+root.resizable(width=True, height=True)
+root.minsize(width=500, height=300)
+root.mainloop()

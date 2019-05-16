c2f5d4c93262ea445bc8f86b3242f3cf95a443ea
Thuy Nguyen <newuser886@yahoo.com>
2019-05-16 15:47:41 +0700

    Bai 95

--- centerlib.py
+++ centerlib.py
@@ -1,3 +1,30 @@
+def TaoDB(dbname: str):
+    import mysql.connector
+    db = mysql.connector.connect(host='113.161.84.155', user='root', passwd='khongco')
+    sql = "CREATE DATABASE {0}".format(dbname)
+    cursor = db.cursor()
+    cursor.execute(sql)
+    print("Da Tao Database {0}".format(dbname))
+    cursor.close()
+    db.close()
+
+
+def TaoTable(name: str):
+    pass
+
+
+def ThemRec():
+    pass
+
+
+def XoaRec():
+    pass
+
+
+def UpdateRec():
+    pass
+
+
 def makecenter(root):
     root.update_idletasks()
     width = root.winfo_width()
